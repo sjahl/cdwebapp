050f2538b83995d6fcceff4178c4f2b19fbf313e
sjahl <sjahl@broadinstitute.org>
2021-02-24 10:13:02 -0500

    adds a basic unit test

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSysinfo(t *testing.T) {
+	got := Sysinfo("localhost", "0.0.a")
+	want := "<h1>Hello</h1>\n<p>My hostname is: localhost</p>\n<p>My version is: 0.0.a</p>\n"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
