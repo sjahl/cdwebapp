cd2df9478de7406b42065ecf3d907f65f6765084
sjahl <sjahl@broadinstitute.org>
2021-06-01 10:47:24 -0400

    add some logging facilities to test gcp logging semantics

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-const version = "0.0.12"
+const version = "0.0.13"
+
+var (
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+)
 
 // Sysinfo accepts a hostname and version as strings and returns a formatted string
 func Sysinfo(hostname, version string) string {
@@ -16,11 +22,25 @@ func Sysinfo(hostname, version string) string {
 
 func main() {
 
+	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	InfoLogger.Println("Starting up...")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		hostname, _ := os.Hostname()
 
+		if strings.HasSuffix(r.URL.Path, "favicon.ico") {
+			ErrorLogger.Println("Favicon requested: stop asking for my favicon!")
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		InfoLogger.Println("Favicon not requested: Genetrating response")
 		responseText := Sysinfo(hostname, version)
 		fmt.Fprintf(w, responseText)
+		InfoLogger.Println("Favicon not requested: Rendered page. finishing.")
+		ErrorLogger.Println("Something is strange, better send a message to stderr")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
